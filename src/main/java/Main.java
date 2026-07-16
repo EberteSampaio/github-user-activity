43be30fc87b7a864ba3b30adb3be748ae0082a84
EberteSampaio <deveberte@gmail.com>
2026-07-16 19:05:54 -0300

    feat: accept arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,15 @@ import command.CommandExecutor;
 import command.impl.FetchGithubActivityUserCommand;
 
 void main(String[] args) {
-    CommandExecutor commandExecutor = new CommandExecutor();
+    try{
+        if (args.length == 0){
+            throw new RuntimeException("Username is required");
+        }
+        CommandExecutor commandExecutor = new CommandExecutor();
 
-    commandExecutor.run(new FetchGithubActivityUserCommand(args[0]));
+        commandExecutor.run(new FetchGithubActivityUserCommand(args[0]));
+    }catch (RuntimeException e){
+        System.err.println(e.getMessage());
+    }
 
 }

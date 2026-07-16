1f515241ff608d05c08d9a5db35db5b40e6a2088
EberteSampaio <deveberte@gmail.com>
2026-07-16 16:03:24 -0300

    feat: create name EventType

--- src/main/java/domain/EventType.java
+++ src/main/java/domain/EventType.java
@@ -1,20 +1,41 @@
 package domain;
 
+
+import java.util.Arrays;
+
 public enum EventType {
-    COMMIT_COMMENT_EVENT,
-    CREATE_EVENT,
-    DELETE_EVET,
-    DISCUSSION_EVENT,
-    FORK_EVENT,
-    GOLLUM_EVENT,
-    ISSUE_COMMENT_EVENT,
-    ISSUE_EVENT,
-    MEMBER_EVENT,
-    PUBLIC_EVENT,
-    PULL_REQUEST_EVENT,
-    PULL_REQUEST_REVIEW_EVENT,
-    PULL_REQUEST_REVIEW_COMMENT_EVENT,
-    PUSH_EVENT,
-    RELEASE_EVENT,
-    WATCH_EVENT,
+    COMMIT_COMMENT_EVENT("CommitCommentEvent"),
+    CREATE_EVENT("CreateEvent"),
+    DELETE_EVET("DeleteEvent"),
+    DISCUSSION_EVENT("DiscussionEvent"),
+    FORK_EVENT("ForkEvent"),
+    GOLLUM_EVENT("GollumEvent"),
+    ISSUE_COMMENT_EVENT("IssueCommentEvent"),
+    ISSUE_EVENT("IssueEvent"),
+    MEMBER_EVENT("MemberEvent"),
+    PUBLIC_EVENT("PublicEvent"),
+    PULL_REQUEST_EVENT("PullRequestEvent"),
+    PULL_REQUEST_REVIEW_EVENT("PullRequestReviewEvent"),
+    PULL_REQUEST_REVIEW_COMMENT_EVENT("PullRequestReviewCommentEvent"),
+    PUSH_EVENT("PushEvent"),
+    RELEASE_EVENT("ReleaseEvent"),
+    WATCH_EVENT("WatchEvent");
+
+
+    private String name;
+
+    EventType(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static EventType fromName(String name) {
+        return Arrays.stream(EventType.values())
+                .filter(v -> v.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Event type not found"));
+    }
 }

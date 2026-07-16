bbf4489fbd6d0248547627cf913671540b0c5fcb
EberteSampaio <deveberte@gmail.com>
2026-07-16 15:59:39 -0300

    feat: remove Payload.java, because is not a fixed payload

--- src/main/java/domain/Payload.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class Payload {
-    private long repositoryId;
-    private long pushedId;
-    private String ref;
-    private String head;
-    private String before;
-}
